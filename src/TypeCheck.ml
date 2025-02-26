684327ebd6b4ecd812541fe9d674c5f1e26388b0
Dark Hole <dark.hole1@yandex.ru>
2025-02-26 17:30:20 +0300

    Add errors list

--- src/TypeCheck.ml
+++ src/TypeCheck.ml
@@ -1,3 +1,33 @@
+type tyError =
+  MissingMain
+  | UndefinedVariable
+  | UnexpectedTypeOfExpression
+  | NotAFunction
+  | NotATuple
+  | NotARecord
+  | NotAList
+  | UnexpectedLambda
+  | UnexpectedTypeForParameter
+  | UnexpectedTuple
+  | UnexpectedRecord
+  | UnexpectedVariant
+  | UnexpectedList
+  | UnexpectedInjection
+  | MissingRecordFields
+  | UnexpectedRecordFields
+  | UnexpectedFieldAccess
+  | UnexpectedVariantLabel
+  | TupleIndexOutOfBounds
+  | UnexpectedTupleLength
+  | AmbiguousSumType
+  | AmbiguousList
+  | IllegalEmptyMatching
+  | NonexhaustiveMatchPatterns
+  | UnexpectedPatternForType
+  | DuplicateRecordFields
+  | DuplicateRecordTypeFields
+  | DuplicateVariantTypeFields
+
 let typecheckProgram (program : AbsStella.program) =
   Printf.printf "typechecker is not implemented\n"
 
