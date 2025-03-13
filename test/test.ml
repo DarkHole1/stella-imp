5d562638930a02d72dcc1fb523a2518228c8adcf
Dark Hole <dark.hole1@yandex.ru>
2025-03-13 11:33:17 +0300

    Add basic operations test

--- test/test.ml
+++ test/test.ml
@@ -56,6 +56,34 @@ let test_nested_typecheck () =
     "inl(<| hello = [true, false] |>)"
     "<| hello : [Bool] |> + <| world : {{ c : Unit }} |>"
 
+let test_operations_typecheck () =
+  check_typecheck' "succ(0) <= Nat" "succ(0)" "Nat";
+  check_typecheck' "Nat::pred(0) <= Nat" "Nat::pred(0)" "Nat";
+  check_typecheck' "Nat::iszero(0) <= Bool" "Nat::iszero(0)" "Bool";
+
+  check_typecheck' "{ 0 }.1 <= Nat" "{ 0 }.1" "Nat";
+  check_typecheck' "{ 0, true }.2 <= Bool" "{ 0, true }.2" "Bool";
+  check_typecheck' "{ 0, true, unit }.2 <= Bool" "{ 0, true, unit }.2" "Bool";
+
+  check_typecheck' "{ a = 0, b = unit, foo = true }.a <= Nat"
+    "{ a = 0, b = unit, foo = true }.a" "Nat";
+  check_typecheck' "{ a = 0, b = unit, foo = true }.b <= Unit"
+    "{ a = 0, b = unit, foo = true }.b" "Unit";
+  check_typecheck' "{ a = 0, b = unit, foo = true }.foo <= Bool"
+    "{ a = 0, b = unit, foo = true }.foo" "Bool";
+
+  check_typecheck' "true as Bool <= Bool" "true as Bool" "Bool";
+  check_typecheck' "inl(0) as (Nat + Unit) <= Nat + Unit" "inl(0) as (Nat + Unit)"
+    "Nat + Unit";
+  check_typecheck' "inr(unit) as (Nat + Unit) <= Nat + Unit"
+    "inr(unit) as (Nat + Unit)" "Nat + Unit";
+  check_typecheck'
+    "<| a = 0 |> as <| a : Nat, b : Bool |> <= <| a : Nat, b : Bool |>"
+    "<| a = 0 |> as <| a : Nat, b : Bool |>" "<| a : Nat, b : Bool |>";
+  check_typecheck'
+    "<| b = false |> as <| a : Nat, b : Bool |> <= <| a : Nat, b : Bool |>"
+    "<| b = false |> as <| a : Nat, b : Bool |>" "<| a : Nat, b : Bool |>"
+
 let test_basic_infer () =
   check_infer' "true => Bool" "Bool" "true";
   check_infer' "false => Bool" "Bool" "false";
@@ -87,6 +115,34 @@ let test_nested_infer () =
     "{a = {true, 0}, b = [unit, unit]} <= {a : {Bool, Nat}, b : [Unit]}"
     "{a : {Bool, Nat}, b : [Unit]}" "{a = {true, 0}, b = [unit, unit]}"
 
+let test_operations_infer () =
+  check_infer' "succ(0) => Nat" "Nat" "succ(0)";
+  check_infer' "Nat::pred(0) => Nat" "Nat" "Nat::pred(0)";
+  check_infer' "Nat::iszero(0) => Bool" "Bool" "Nat::iszero(0)";
+
+  check_infer' "{ 0 }.1 => Nat" "Nat" "{ 0 }.1";
+  check_infer' "{ 0, true }.2 => Bool" "Bool" "{ 0, true }.2";
+  check_infer' "{ 0, true, unit }.2 => Bool" "Bool" "{ 0, true, unit }.2";
+
+  check_infer' "{ a = 0, b = unit, foo = true }.a => Nat" "Nat"
+    "{ a = 0, b = unit, foo = true }.a";
+  check_infer' "{ a = 0, b = unit, foo = true }.b => Unit" "Unit"
+    "{ a = 0, b = unit, foo = true }.b";
+  check_infer' "{ a = 0, b = unit, foo = true }.foo => Bool" "Bool"
+    "{ a = 0, b = unit, foo = true }.foo";
+
+  check_infer' "true as Bool => Bool" "Bool" "true as Bool";
+  check_infer' "inl(0) as (Nat + Unit) => Nat + Unit" "Nat + Unit"
+    "inl(0) as (Nat + Unit)";
+  check_infer' "inr(unit) as (Nat + Unit) => Nat + Unit" "Nat + Unit"
+    "inr(unit) as (Nat + Unit)";
+  check_infer'
+    "<| a = 0 |> as <| a : Nat, b : Bool |> => <| a : Nat, b : Bool |>"
+    "<| a : Nat, b : Bool |>" "<| a = 0 |> as <| a : Nat, b : Bool |>";
+  check_infer'
+    "<| b = false |> as <| a : Nat, b : Bool |> => <| a : Nat, b : Bool |>"
+    "<| a : Nat, b : Bool |>" "<| b = false |> as <| a : Nat, b : Bool |>"
+
 let () =
   Alcotest.run "Typecheck"
     [
@@ -101,5 +157,9 @@ let () =
           Alcotest.test_case "Typecheck nested types" `Quick
             test_nested_typecheck;
           Alcotest.test_case "Infer nested types" `Quick test_nested_infer;
+          Alcotest.test_case "Typecheck basic operations" `Quick
+            test_operations_typecheck;
+          Alcotest.test_case "Infer basic operations" `Quick
+            test_operations_infer;
         ] );
     ]
